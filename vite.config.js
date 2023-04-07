be44b183684f42b65b087fc7cc3a095764edc28f
fdoganis <fivos.doganis@gmail.com>
2023-04-07 10:45:13 +0200

    enable sourcemaps in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,17 @@ import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
     clearScreen: false,
+    build: {
+        sourcemap: true
+    },
     server: {
         open: true
     },
     plugins: [
         viteStaticCopy({
           targets: [
-            { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
-			      { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' }
+              { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
+	      { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' }
           ]
         })
       ]

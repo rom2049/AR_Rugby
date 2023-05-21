aab60aa613cad1733ef8688bdcf51333109bb5e6
fdoganis <fivos.doganis@gmail.com>
2023-05-21 18:30:33 +0200

    Update vite.config.js: Draco support

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,11 @@ export default defineConfig({
         viteStaticCopy({
           targets: [
               { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.js', dest: 'jsm/libs/' },
-	      { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' }
+	      { src: 'node_modules/three/examples/jsm/libs/ammo.wasm.wasm', dest: 'jsm/libs/' },
+	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.js', dest: 'jsm/libs/draco/gltf' },
+	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_decoder.wasm', dest: 'jsm/libs/draco/gltf/' },
+	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_encoder.js', dest: 'jsm/libs/draco/gltf/' },
+	      { src: 'node_modules/three/examples/jsm/libs/draco/gltf/draco_wasm_wrapper.js', dest: 'jsm/libs/draco/gltf/' }
           ]
         }),
         glsl()
